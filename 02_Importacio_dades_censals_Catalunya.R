1f5ddd3f4c9d29c053192b5af7a7c36f2fddf5cf
jordianguera <jordi.angueracos@gmail.com>
2026-01-07 12:09:20 +0100

    Corregides les columnes provincia, municipi, districte i secció. I afegit filtre d'any i provincia

--- 02_Importacio_dades_censals_Catalunya.R
+++ 02_Importacio_dades_censals_Catalunya.R
@@ -8,32 +8,25 @@ fitxers <- c("10020003.DAT", "10020403.DAT", "10020803.DAT", "10021111.DAT",
 
 processa <- function(path) {
   lines <- readLines(path)
-  
   df <- data.frame(
-    proces      = substr(lines, 1, 2),
-    any         = substr(lines, 3, 6),
-    mes         = substr(lines, 7, 8),
-    ccaa        = substr(lines, 9, 10),
-    provincia   = substr(lines, 11, 12),
-    municipi    = substr(lines, 13, 15),
-    districte   = substr(lines, 16, 18),
-    seccio      = substr(lines, 19, 21),
-    tipus       = substr(lines, 23, 23),
+    proces = substr(lines, 1, 2),
+    any = substr(lines, 3, 6),
+    mes = substr(lines, 7, 8),
+    volta = substr(lines, 7, 8),
+    ccaa = substr(lines, 10, 11),
+    provincia = substr(lines, 12, 13),
+    municipi = substr(lines, 12, 16),
+    districte = substr(lines, 12, 18),
+    seccio = substr(lines, 12, 21),
+    tipus = substr(lines, 23, 23),
     candidatura = substr(lines, 24, 29),
-    vots        = as.integer(substr(lines, 30, 36)),
+    vots = as.integer(substr(lines, 30, 36)),
     stringsAsFactors = FALSE
   )
   
-  df <- df[df$provincia %in% c("08", "17", "25", "43"), ]
+  df <- df[df$provincia %in% c("08", "17", "25","43"), ]
+  df <- df[df$any %in% c("2023", "2019", "2016", "2015"), ]
   
-  df$codi <- paste0(
-    sprintf("%02s", df$provincia),
-    sprintf("%03s", df$municipi),
-    sprintf("%02s", df$districte),
-    sprintf("%03s", df$seccio)
-  )
-  
-  df
 }
 
 for (f in fitxers) {
@@ -45,5 +38,5 @@ for (f in fitxers) {
 llista_df <- lapply(fitxers, processa)
 df_total <- do.call(rbind, llista_df)
 
-# saveRDS(df_total, "resultats_totals.rds")
+saveRDS(df_total, "resultats_totals.rds")
 
